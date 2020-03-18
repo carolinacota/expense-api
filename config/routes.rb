51ae74931f9589a4da0314318b73d1e44d1292d7
Carolina Cota <carolinareycota@gmail.com>
2020-03-18 19:53:20 +0100

    Update routes.rb
    routes configured.

--- config/routes.rb
+++ config/routes.rb
@@ -1,14 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
-  root to: 'pages#home'
-  end
 
-
-Rails.application.routes.draw do
-
+  # devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :transactions
+      resources :transactions, only: [ :index, :show ]
     end
   end
 end
