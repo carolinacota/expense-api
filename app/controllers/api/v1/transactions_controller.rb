a8c719c7db20af343f4ea4a60480400f79cc8d15
Carolina Cota <carolinareycota@gmail.com>
2020-03-18 19:53:06 +0100

    added set_transaction to controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,5 +1,17 @@
 class Api::V1::TransactionsController < Api::V1::BaseController
+  before_action :set_transaction, only: [ :show ]
+
+  def show
+  end
+
   def index
     @transactions = policy_scope(Transaction)
+  end
+
+  private
+
+  def set_transaction
+    @transaction = Transaction.find(params[:id])
+    authorize @transaction  # For Pundit
   end
 end
