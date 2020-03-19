eee344f16907c6fd0040837afc5c11e784e98463
Carolina Cota <carolinareycota@gmail.com>
2020-03-19 16:59:49 +0100

    Seeds file created

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,30 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+Category.destroy_all
+User.destroy_all
+Transaction.destroy_all
+
+puts "Creating Categories"
+health = Category.create(name: "health", color: "blue")
+home = Category.create(name: "home", color: "red")
+clothing = Category.create(name: "clothing", color: "black")
+education = Category.create(name: "education", color: "white")
+entertainment = Category.create(name: "entertainment", color: "pink")
+foodAndDrink = Category.create(name: "food and drink", color: "purple")
+supermarket = Category.create(name: "supermarket", color: "green")
+transportation = Category.create(name: "transportation", color: "orange")
+
+puts "Creating Users"
+carol = User.create(email: "carolinareycota@gmail.com", password: "12345678")
+
+puts "Creating Transactions"
+t1 = Transaction.create(value: 33, currency: "euro", paid_on: "01/03/2020",
+                        description: "health insurance", category: health,
+                        user: carol)
+
+t2 = Transaction.create(value: 59, currency: "euro", paid_on: "18/03/2020",
+                        description: "mercadona", category: supermarket,
+                        user: carol)
+
+t3 = Transaction.create(value: 1100, currency: "euro", paid_on: "16/03/2020",
+                        description: "rent + bills", category: home,
+                        user: carol)
+
