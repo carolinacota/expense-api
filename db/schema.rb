56d834d215ccb741e958f3be244a24d4712a3c4c
Carolina Cota <carolinareycota@gmail.com>
2020-03-19 17:00:37 +0100

    set email to be unique in db

--- db/schema.rb
+++ db/schema.rb
@@ -36,7 +36,7 @@ ActiveRecord::Schema.define(version: 2020_03_19_141637) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
+    t.string "email", default: "", null: false, unique: true
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
