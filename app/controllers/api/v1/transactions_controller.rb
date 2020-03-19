31f7b5a95c827113e3d1c76012d770ee337bafe0
Carolina Cota <carolinareycota@gmail.com>
2020-03-19 15:36:27 +0100

    added create method to transactions controller

--- app/controllers/api/v1/transactions_controller.rb
+++ app/controllers/api/v1/transactions_controller.rb
@@ -1,11 +1,26 @@
 class Api::V1::TransactionsController < Api::V1::BaseController
-  before_action :set_transaction, only: [ :show ]
+  before_action :set_transaction, only: [ :show, :update ]
+
+  def index
+    @transactions = policy_scope(Transaction)
+  end
 
   def show
   end
 
-  def index
-    @transactions = policy_scope(Transaction)
+  def create
+    @transaction = Transaction.new(transaction_params)
+    @transaction.user = current_user
+    @transaction.category = category
+    authorize @transaction
+    if @transaction.save
+      render :show, status: :created
+    else
+      render_error
+    end
+  end
+
+  def update
   end
 
   private
@@ -14,4 +29,9 @@ class Api::V1::TransactionsController < Api::V1::BaseController
     @transaction = Transaction.find(params[:id])
     authorize @transaction  # For Pundit
   end
+
+  def transaction_params
+    params.require(:transaction).permit(:paid_on, :currency, :value, :description)
+  end
+
 end
