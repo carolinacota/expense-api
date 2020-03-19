1eac73f19ef18ca424b8b9df9987222cc1cc9162
Carolina Cota <carolinareycota@gmail.com>
2020-03-19 15:34:41 +0100

    Update routes.rb
    added more actions to routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
 
-  # devise_for :users
+  devise_for :users
   namespace :api, defaults: { format: :json } do
     namespace :v1 do
-      resources :transactions, only: [ :index, :show ]
+      # resources :category, only: [:show, :index] do
+      resources :transactions, only: [ :index, :show, :create, :update, :destroy ]
+      # end
     end
   end
 end
