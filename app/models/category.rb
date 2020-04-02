7611ad0cba0e8bf132aa54e6c200c73cff577d9c
Carolina Cota <carolinareycota@gmail.com>
2020-04-02 11:10:52 +0200

    fix categories sum to run seeds file.

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,12 +1,13 @@
 class Category < ApplicationRecord
   has_many :transactions, dependent: :destroy
 
-  monetize :sum_of_transactions_cents, with_model_currency: :currency
-  monetize :sum_of_transactions_in_month_cents, with_model_currency: :currency
+  # monetize :sum_of_transactions_cents, with_model_currency: :currency
+  # monetize :sum_of_transactions_in_month_cents, with_model_currency: :currency
 
-  def sum_of_transactions_cents
-    transactions.sum(:price_cents)
-  end
+  # def sum_of_transactions_cents
+  #   transactions.sum(:price_cents)
+  # end
+
 
   def sum_of_transactions_in_month_cents(month, user)
     transactions.where("EXTRACT(MONTH FROM paid_on) = ? AND user_id = ? ", month, user)
